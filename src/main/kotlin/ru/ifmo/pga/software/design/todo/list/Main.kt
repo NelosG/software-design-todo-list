7ea5b77d6e8276aef2105689021c1f9253b2d493
gpushkarev <gleb.pushkarev@gmail.com>
2022-11-21 11:01:17 +0300

    move core functionality into core module

---
+++ src/main/kotlin/ru/ifmo/pga/software/design/todo/list/Main.kt
@@ -0,0 +1,7 @@
+package ru.ifmo.pga.software.design.todo.list
+
+import org.springframework.boot.SpringApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
